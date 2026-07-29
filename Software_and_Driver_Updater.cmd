c34b18da69f1fa2272c6c86fc10b5938526e41a7
Richard Kelsch <richcsst@users.noreply.github.com>
2026-07-28 19:49:34 -0600

    Update title for Windows 10/11 compatibility

--- Software_and_Driver_Updater.cmd
+++ Software_and_Driver_Updater.cmd
@@ -58,7 +58,7 @@ set "REPO_URL=https://github.com/richcsst/HandyWindowsUtilities"
 set "URL_LINK=%BRIGHT_MAGENTA%%REPO_URL%%RESET%"
 set "DIVIDER=%BG_BLACK%%BRIGHT_BLUE%=====================================================================================================================%RESET%"
 
-title Windows 11 Software ^& Driver Updater
+title Windows 10/11 Software ^& Driver Updater
 
 :: ------------------------------------------------------------------------
 :: Clear Screen Routine
